4be7aa8e570267f185e7ff2d153f8dd6d5eb7422
Iwo <igi31@wp.pl>
2024-07-31 21:09:15 +0200

    Added new code from lessons

---
+++ udemy_kurs_4-srednik_wartosci_przyklad.py
@@ -0,0 +1,11 @@
+# Przypisanie zmiennych po średniku:
+
+x = 5; y = 8; z = 10
+
+# Przypisanie zmiennych po przecinku, kolejno do a, b i do c:
+
+a, b, c = 1, 5, 15
+
+# Można przypisywać również różne wartości do zmiennych co jest dosyć oczywiste:
+
+d, e, f = 1, "blabla", True

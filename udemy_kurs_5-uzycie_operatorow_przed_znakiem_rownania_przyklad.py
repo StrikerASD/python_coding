27a53be6c544d14e1472d04f0c83f49140d4da81
Iwo <igi31@wp.pl>
2024-08-01 17:50:37 +0200

    Added new code from lessons

---
+++ udemy_kurs_5-uzycie_operatorow_przed_znakiem_rownania_przyklad.py
@@ -0,0 +1,17 @@
+# Definiowanie zmiennej przy każdym działaniu jest potrzebne żeby zresetować wartość zmiennej.
+
+x = 5
+
+x += 10
+print(x)
+
+x = 5
+
+x -= 2
+print(x)
+
+x = 5
+
+x *= 9
+print(x)
+

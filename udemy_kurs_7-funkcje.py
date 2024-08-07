0c8e6ad5175a4df34f35e50b553a3e3e29e81886
Iwo <igi31@wp.pl>
2024-08-07 18:24:43 +0200

    Added new code from lessons

--- udemy_kurs_7-funkcje.py
+++ udemy_kurs_7-funkcje.py
@@ -1,15 +1,15 @@
 # Importowanie  biblioteki math. Potem używając math wpisujemy funkcję której chcemy użyć, np.: math.sqrt(wartość)
 
 import math
-math.sqrt(4)
+print(math.sqrt(4))
 
 # Importowanie funkcji sqrt, ceil z biblioteki math. Potem wystarczy wpisać nazwę funkcji i podać wartość jak w przykładzie poniżej.
 
 from math import sqrt
 from math import ceil
 
-sqrt(10)
-ceil(5)
+print(sqrt(10))
+print(ceil(5))
 
 # Importowanie wszystkich funkcji z biblioteki math
 
---
+++ udemy_kurs_8-funkcje-tekstowe.py
@@ -0,0 +1,12 @@
+# Funkcje tekstowe - zmniejszanie i kapitalizacja liter
+
+imie = "iwo"
+
+print(imie.capitalize())
+
+imieKaroliny = "karolina"
+
+imieKaroliny = imieKaroliny.capitalize()
+
+print(imieKaroliny)
+

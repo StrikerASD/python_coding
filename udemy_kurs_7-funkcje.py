dd4dbf664c3dc73d241138c745e8943d9424f581
Iwo <igi31@wp.pl>
2024-08-07 18:09:00 +0200

    Added new code from lessons

--- udemy_kurs_7-funkcje.py
+++ udemy_kurs_7-funkcje.py
@@ -1,2 +1,17 @@
+# Importowanie  biblioteki math. Potem używając math wpisujemy funkcję której chcemy użyć, np.: math.sqrt(wartość)
+
 import math
 math.sqrt(4)
+
+# Importowanie funkcji sqrt, ceil z biblioteki math. Potem wystarczy wpisać nazwę funkcji i podać wartość jak w przykładzie poniżej.
+
+from math import sqrt
+from math import ceil
+
+sqrt(10)
+ceil(5)
+
+# Importowanie wszystkich funkcji z biblioteki math
+
+from math import *
+

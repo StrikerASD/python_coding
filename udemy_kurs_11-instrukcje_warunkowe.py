9b83abeec3922da733b8e722c7c9b170d6c3f565
Iwo <igi31@wp.pl>
2024-08-11 19:09:35 +0200

    Added new code from lessons

--- udemy_kurs_11-instrukcje_warunkowe.py
+++ udemy_kurs_11-instrukcje_warunkowe.py
@@ -5,19 +5,26 @@ print("Wybierz jedną z poniższych opcji:")
 print("Mnożenie - 1 Dzielenie - 2 Dodawanie - 3 Odejmowanie - 4:")
 wybor = int(input())
 
-a = int(input("Wybierz pierwszą liczbę: "))
-b = int(input("Wybierz drugą liczbę: "))
-
 if (wybor == 1):
+    a = int(input("Wybierz pierwszą liczbę: "))
+    b = int(input("Wybierz drugą liczbę: "))
     print("Wynik mnożenia: ", a, "*", b, "=", (a * b))
 elif (wybor == 2):
+    a = int(input("Wybierz pierwszą liczbę: "))
+    b = int(input("Wybierz drugą liczbę: "))
     if (b == 0):
         print("Cholero, nie dziel przez zero!")
     else:
         print("Wynik dzielenia: ", a, "/", b, "=", (a / b))
 
 elif (wybor == 3):
+    a = int(input("Wybierz pierwszą liczbę: "))
+    b = int(input("Wybierz drugą liczbę: "))
     print("Wynik dodawania: ",a, "+", b, "=", (a + b))
 
 elif (wybor == 4):
+    a = int(input("Wybierz pierwszą liczbę: "))
+    b = int(input("Wybierz drugą liczbę: "))
     print("Wynik odejmowania: ",a, "-", b, "=", (a - b))
+else:
+    print("Dokonałeś złego wyboru")

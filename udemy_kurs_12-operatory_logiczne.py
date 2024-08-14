df79ed7e2a0b43f53c9aa4dc8b67f56e6c5723ca
izaleski <iwo.zaleski@payback.net>
2024-08-14 15:32:31 +0200

    Added new python code

--- udemy_kurs_12-operatory_logiczne.py
+++ udemy_kurs_12-operatory_logiczne.py
@@ -1,6 +1,15 @@
 warunek = int(input("Podaj liczbę z zakresu od 1 do 10: "))
-if(warunek >= 1 and warunek <= 10):
-    print("Podana liczba jest z zakresu od 1 do 10.")
-    print("Wpisałeś ", warunek)
+# if(warunek >= 1 and warunek <= 10):
+#     print("Podana liczba jest z zakresu od 1 do 10.")
+#     print("Wpisałeś ", warunek)
+# else:
+#     print("Liczba poza uzgodnionym zakresem: ", warunek)
+#
+# if(warunek >= 1 or warunek <= 10):
+#     print("Jedna z podanych liczb jest z zakresu od 1 do 10 lub wyżej.")
+#     print("Wpisałeś ", warunek)
+
+if(not(warunek >= 1 and warunek <= 10)):
+    print("Wpisana liczba jest... ", warunek)
 else:
-    print("Liczba poza uzgodnionym zakresem: ", warunek)
+    print("Wpisana liczba jest spoza zakresu...")
